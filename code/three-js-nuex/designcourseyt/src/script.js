be3efdf868536dce77318ba2cc4f705042b341e5
Vaughn <44816847+va-code@users.noreply.github.com>
2021-12-19 10:37:30 -0600

    Getting a grasp of website and automation
    threejs ideas for how to build the website

---
+++ code/three-js-nuex/designcourseyt/src/script.js
@@ -0,0 +1,113 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+
+//texture
+const texture = new THREE.TextureLoader().load("textures/water.jpg")
+
+
+// Materials
+
+const material = new THREE.MeshBasicMaterial({color:0xff75fc})
+const material1 = new THREE.MeshBasicMaterial({color:0xfffff})
+material.color = new THREE.Color(0xffffff)
+material1.color = new THREE.Color(0xff75a3)
+
+// Mesh
+const torus = new THREE.Mesh(geometry,material)
+const torus2 = new THREE.Mesh(geometry,material1)
+scene.add(torus)
+scene.add(torus2)
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xff0000, 0.1)
+pointLight.position.x = 3
+pointLight.position.y = 3
+pointLight.position.z = 3
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    torus.rotation.y = 1 * elapsedTime
+    torus2.rotation.y = -3.141595 * elapsedTime
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
--- code/three-js/designcourseyt/src/script.js
+++ code/three-js/designcourseyt/src/script.js
@@ -15,21 +15,29 @@ const scene = new THREE.Scene()
 // Objects
 const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
+//texture
+const texture = new THREE.TextureLoader().load("textures/water.jpg")
+
+
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshBasicMaterial({color:0xff75fc})
+const material1 = new THREE.MeshBasicMaterial({color:0xfffff})
+material.color = new THREE.Color(0xffffff)
+material1.color = new THREE.Color(0xff75a3)
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const torus = new THREE.Mesh(geometry,material)
+const torus2 = new THREE.Mesh(geometry,material1)
+scene.add(torus)
+scene.add(torus2)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
+const pointLight = new THREE.PointLight(0xff0000, 0.1)
+pointLight.position.x = 3
 pointLight.position.y = 3
-pointLight.position.z = 4
+pointLight.position.z = 3
 scene.add(pointLight)
 
 /**
@@ -60,14 +68,14 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
+camera.position.x = 2
+camera.position.y = 2
 camera.position.z = 2
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -90,8 +98,8 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
+    torus.rotation.y = 1 * elapsedTime
+    torus2.rotation.y = -3.141595 * elapsedTime
     // Update Orbital Controls
     // controls.update()
 
